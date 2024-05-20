696ee401e04a2f380db969368c9f240880eeb8af
dokhung <94828988+dokhung@users.noreply.github.com>
2024-05-21 02:43:43 +0900

    20240521

--- Assets/0.Script/Door/InBoss.cs
+++ Assets/0.Script/Door/InBoss.cs
@@ -2,28 +2,28 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InBoss : MonoBehaviour
 {
     public Transform InBossTr;
     private bool InPotal = false;
 
+    
+
     private void OnTriggerEnter2D(Collider2D other) 
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            Debug.Log("보스한테 갈수있음 위를 누르세요");
             InPotal = true;
-
         }
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha3) && InPotal)
+        if (Input.GetKeyDown(KeyCode.UpArrow) && InPotal)
         {
             InputManager.Instance.transform.position = InBossTr.transform.position;
-            Debug.Log("보스와 접견 성능");
         }
     }
 }
